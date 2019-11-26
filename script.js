be7552a1960a487b3062e4a9bd97f01ef82e93ca
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2019-11-26 17:46:16 +0530

    chat_app
    This is my first node chat app

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 const socket = io('http://localhost:3000')
+const messageForm = document.getElementById('send-container')
+
+const name = prompt('What is your name?')
+appendMessage('You joined')
+socket.emit('new-user', name)
+
 
 socket.on('chat-message', data => {
-    console.log(data)
+    appendMessage('${date.name}: ${data.message}')
 })
+
+socket.on('user-connected', name => {
+    appendMessage('${name} connected')
+})
+
+socket.on('user-disconeected', name => {
+    appendMessage('${name} disconnected')
+})
+
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage('$You: ${message}')
+    socket.emit('send-chat-message', message)
+    messageInput.value = ''
+})
+
+function appendMessage(message) {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
